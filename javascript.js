bfff79a811621eda9cbf92d96576de28abcd9fa6
Raventh23 <blazenether@gmail.com>
2024-06-11 19:41:13 -0400

    create new function to decide which operation add the variables that will be used for user selection

--- javascript.js
+++ javascript.js
@@ -10,3 +10,26 @@ function multi(a, b){
 function divide(a, b){
     return a / b;
 };
+function operate(a ,b, operand){
+    if(operand == '+'){
+        return add(a, b);
+    }
+    else if(operand == '-'){
+        return sub(a, b);
+    }
+    else if(operand == '*'){
+        return multi(a, b);
+    }
+    else if(operand == '/'){
+        if(a == 0 && b == 0){
+            alert("what are you thinking?");
+        }
+        else{
+            return divide(a,b);
+        }
+    }
+}
+let numberA;
+let numberB;
+let operand;
+console.log(operate(5, 7, '/'));
